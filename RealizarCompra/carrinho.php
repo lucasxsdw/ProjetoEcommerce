9b3dd06034db8d51c6653f06e881b57c5471d799
lucas Ryan <133159394+lucasxsdw@users.noreply.github.com>
2025-02-05 01:02:21 -0300

    carrinho

--- RealizarCompra/carrinho.php
+++ RealizarCompra/carrinho.php
@@ -23,15 +23,3 @@ if (isset($_SESSION['carrinho']) && is_array($_SESSION['carrinho']) && count($_S
 
     
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <button>Excluir</button>
-</body>
-</html>
