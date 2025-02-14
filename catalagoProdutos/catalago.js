29742fa65af95a10f691a76380c562781146f1a3
lucas Ryan <133159394+lucasxsdw@users.noreply.github.com>
2025-02-14 16:30:39 -0300

    carrinho

--- catalagoProdutos/catalago.js
+++ catalagoProdutos/catalago.js
@@ -1,4 +1,4 @@
-/*const carrinho = document.querySelector(".carrinho")
+const carrinho = document.querySelector(".carrinho")
 const button = document.querySelector(".cart-button")
 const fechar = document.querySelector(".fecharCarrinho")
 
@@ -13,20 +13,3 @@ fechar.addEventListener("click", () => {
 
 
 
- <form id="name-form" action="" method="post">
-    <input type="hidden" name="produto_id" value="1">
-    <input type="hidden" name="produto_nome" value="Camisa oversize - All black">
-    <input type="hidden" name="produto_preco" value="R$130,00">
-        input type="hidden" name="produto_imagem" value="../img/img.webp">
-       <button type="submit" class="cart-button">
-    <img src="../img/carrinho.png" alt="Carrinho" class="cart-icon"> Carrinho
-         </button>
-               
-                </form>
-
-
-*/
-
-const nameForm = document.querySelector("#name-form");
-const containerProduct = document.querySelector(".produtosCarrinho");
-consta
